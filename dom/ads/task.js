cd5e5bb2c020385e4dfee7d0d8feed5480d5e5a9
Sultan Abdulazizov <sultann1.sa66@gmail.com>
2023-11-23 02:16:58 +0300

    dom/ task js dataset-delay added

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,5 +1,6 @@
 const rotatorCase = document.querySelectorAll('.rotator__case');
 const bookContent = document.querySelector('div.card');
+let delay = 1000;
 
 function getIndex(){
     for(let i = 0; i < rotatorCase.length; i++) {
@@ -8,6 +9,8 @@ function getIndex(){
     let index = Math.floor(Math.random() * rotatorCase.length);
        rotatorCase[index].classList.add('rotator__case_active');
        rotatorCase[index].style.color =  rotatorCase[index].dataset.color;
+       delay = rotatorCase[index].dataset.speed;
+       console.log(delay);
        
 }
-setInterval(() => getIndex(), 1000);
+setInterval(() => getIndex(), delay);
